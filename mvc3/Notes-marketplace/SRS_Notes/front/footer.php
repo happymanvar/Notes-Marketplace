bf99f01e41c3539c914c7ad5c7fca7b50d4eb2c9
happymanvar <59997285+happymanvar@users.noreply.github.com>
2021-04-19 22:40:03 +0530

    get media links from database dynamically.

--- mvc3/Notes-marketplace/SRS_Notes/front/footer.php
+++ mvc3/Notes-marketplace/SRS_Notes/front/footer.php
@@ -1,3 +1,27 @@
+<?php
+
+$fb_url = mysqli_query($conn, "SELECT value FROM systemconfigurations WHERE configurationkey = 'facebookurl'");
+if (!($fb_url)) {
+    die("QUERY FAILED" . mysqli_error($conn));
+}
+$fb_url = mysqli_fetch_row($fb_url);
+$fb_url = $fb_url[0];
+$twitter_url = mysqli_query($conn, "SELECT value FROM systemconfigurations WHERE configurationkey = 'twitterurl'");
+if (!($twitter_url)) {
+    die("QUERY FAILED" . mysqli_error($conn));
+}
+$twitter_url = mysqli_fetch_row($twitter_url);
+$twitter_url = $twitter_url[0];
+$linkedin_url = mysqli_query($conn, "SELECT value FROM systemconfigurations WHERE configurationkey = 'linkedinurl'");
+if (!($linkedin_url)) {
+    die("QUERY FAILED" . mysqli_error($conn));
+}
+$linkedin_url = mysqli_fetch_row($linkedin_url);
+$linkedin_url = $linkedin_url[0];
+
+?>
+
+
 <!-- Section Footer -->
 <footer>
     <div class="container">
@@ -11,9 +35,9 @@
             <!-- Social Icon -->
             <div class="col-md-5 col-sm-4 foot-icon text-right">
                 <ul class="social-list">
-                    <li><a href="#"><img src="images/User-Profile/facebook.png" alt="facbook"></a></li>
-                    <li><a href="#"><img src="images/User-Profile/twitter.png" alt="twitter"></a></li>
-                    <li><a href="#"><img src="images/User-Profile/linkedin.png" alt="linkedin"></a></li>
+                    <li><a href="<?php echo $fb_url; ?>"><img src="images/User-Profile/facebook.png" alt="facbook"></a></li>
+                    <li><a href="<?php echo $twitter_url; ?>"><img src="images/User-Profile/twitter.png" alt="twitter"></a></li>
+                    <li><a href="<?php echo $linkedin_url; ?>"><img src="images/User-Profile/linkedin.png" alt="linkedin"></a></li>
                 </ul>
             </div>
 
